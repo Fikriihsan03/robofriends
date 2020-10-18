0e2cb8052b054307155a9e12677993506e0b60a4
fikri <mfikriihsan03@gmail.com>
2020-10-18 10:46:30 +0700

    change the image of card

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,7 @@ import React from 'react';
 const card = ({name,email,id})=>{
     return(
         <div className = 'bg-light-blue dib br3 pa3 ma2 grow bw-5 shadow-2'> 
-            <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+            <img alt="robots" src={require(`../icon/00${id}-robot-${id}.png?200x200`)} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/robots.js
+++ src/robots.js
@@ -1,12 +1,12 @@
 export const robots = [
     {
-      "id": 1,
+      id: 1,
       "name": "Leanne Graham",
       "username": "Bret",
       "email": "Sincere@april.biz",
     },
     {
-      "id": 2,
+      id: 2,
       "name": "Ervin Howell",
       "username": "Antonette",
       "email": "Shanna@melissa.tv",
