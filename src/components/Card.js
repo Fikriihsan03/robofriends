97423fb0489ba670c8b9abb3b1edf5054f1c2612
fikri <mfikriihsan03@gmail.com>
2020-12-15 13:12:31 +0700

    nothing

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,8 @@ import './Card.css'
 const card = ({name,email,id})=>{
     return(
         <div className = 'bg-light-blue dib br3 pa3 ma2 grow bw-5 shadow-2'> 
-            <img className="shadow-5"alt="robots" src={`https://robohash.org/${id}?set=set3`} />
+            <img className="shadow-5
+            "alt="robots" src={`https://robohash.org/${id}?set=set3`} />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
